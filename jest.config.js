e68681c58dd2a1ac4b38c7cae7cab50b9f841681
Maciej Lyskawinski <maciej.lyskawinski@skillandchill.com>
2021-02-25 18:43:14 +0100

    Fixed paths in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns:["node_modules","dist"]
 
 };
